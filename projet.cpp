de465197f58158164d6037226ee9887df89e0fef
Khalil SD <khalilsd2002@gmail.com>
2024-06-03 01:49:04 +0100

    first commit

--- projet.cpp
+++ projet.cpp
@@ -10,7 +10,7 @@
 
 #define NUM_CHILDREN 4
 
-sem_t *sem_start;
+sem_t *semaphore;
 pid_t children[NUM_CHILDREN];
 volatile sig_atomic_t task_signal_received = 0;
 
@@ -19,7 +19,6 @@ void handle_sigusr1(int sig) {
 }
 
 void handle_sigusr2(int sig) {
-    // Signal de confirmation du fils au père
     printf("Père : Signal de confirmation reçu d'un enfant\n");
 }
 
@@ -32,64 +31,52 @@ void child_task(int child_num) {
     signal(SIGUSR1, handle_sigusr1);
     signal(SIGTERM, handle_sigterm);
 
-    // Attendre le signal de départ
     while (!task_signal_received) {
         pause();
     }
 
-    // Tâche simulée par une boucle et un sleep
     printf("Enfant %d : Début de la tâche\n", child_num);
     sleep(2);
     printf("Enfant %d : Tâche terminée\n", child_num);
 
-    // Envoyer le signal de confirmation au père
     kill(getppid(), SIGUSR2);
-
-    // Terminer le processus fils
     exit(0);
 }
 
 int main() {
-    sem_start = sem_open("/sem_start", O_CREAT | O_EXCL, 0644, 0);
-    if (sem_start == SEM_FAILED) {
+    semaphore = sem_open("semaphore", O_CREAT | O_EXCL, 0644, 0);
+    if (semaphore == SEM_FAILED) {
         perror("Échec de sem_open");
         exit(EXIT_FAILURE);
     }
 
     signal(SIGUSR2, handle_sigusr2);
 
-    // Créer les processus fils
     for (int i = 0; i < NUM_CHILDREN; i++) {
         pid_t pid = fork();
         if (pid == -1) {
             perror("Échec de fork");
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
-            // Processus fils
             child_task(i);
         } else {
-            // Processus père
             children[i] = pid;
         }
     }
 
-    // Attendre que tous les fils soient prêts
     for (int i = 0; i < NUM_CHILDREN; i++) {
-        sem_post(sem_start);
+        sem_post(semaphore);
     }
 
-    // Envoyer le signal de départ aux fils une fois qu'ils sont tous prêts
     for (int i = 0; i < NUM_CHILDREN; i++) {
         kill(children[i], SIGUSR1);
     }
 
-    // Attendre que les fils terminent leurs tâches
     for (int i = 0; i < NUM_CHILDREN; i++) {
         wait(NULL);
     }
 
-    // Nettoyer le sémaphore
-    if (sem_unlink("/sem_start") == -1) {
+    if (sem_unlink("semaphore") == -1) {
         perror("Échec de sem_unlink");
         exit(EXIT_FAILURE);
     }
